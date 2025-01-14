9e81dd82d0cd2d13855f6fbb2bdc86d68576d45d
shahed-turkmani <shahed.trk@outlook.com>
2025-01-15 02:02:06 +0400

    great project

---
+++ script/script.js
@@ -0,0 +1,7 @@
+var $grid= $('.xgrid').isotope({});
+$('.filter-button-group').on('click','button',function(){
+
+    var filterValue=$(this).attr('data-filter');
+    $grid.isotope({filter: filterValue});
+
+});
